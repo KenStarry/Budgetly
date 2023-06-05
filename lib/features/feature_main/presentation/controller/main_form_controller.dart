e6a5465ebe316bbb67a9494a301a9fc2e7869909
KenStarry <starrycodes@gmail.com>
2023-06-05 11:12:53 +0300

    Added account balance controller

--- lib/features/feature_main/presentation/controller/main_form_controller.dart
+++ lib/features/feature_main/presentation/controller/main_form_controller.dart
@@ -5,5 +5,11 @@ class MainFormController extends GetxController {
   //  account name controller
   var accountName = "Account Name".obs;
 
+  //  balance controller
+  var accountBalance = 0.00.obs;
+
   void updateAccountName({required String name}) => accountName.value = name;
+
+  void updateAccountBalance({required String balance}) =>
+      accountBalance.value = double.parse(balance);
 }
